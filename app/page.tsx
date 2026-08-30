18b3ff313ab3aed10b6c24ea95048cd80c4f7e64
Matt Davies <matt.davies400@gmail.com>
2026-08-30 07:47:09 -0400

    Add new project images to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const projects = [
   {
     name: "NASA Space Apps",
     description:
-      "Interactive platform for exploring massive space image datasets.",
+      "A hackathon team that built an interactive platform for exploring massive space image datasets.",
     tech: "TypeScript • React",
     link: "https://github.com/davo300/NASA_Space_Apps",
     image: "/projects/nasa-space-apps.jpeg",
@@ -78,6 +78,7 @@ const projects = [
       "C++ heat simulation using OpenCV matrices and the Jacobi method to model temperature diffusion.",
     tech: "C++ • OpenCV",
     link: "https://github.com/davo300/Heat-Simulation",
+    image: "/projects/heat-sim-opencv.png",
   },
   {
     name: "User Registration (Laravel / MySQL)",
